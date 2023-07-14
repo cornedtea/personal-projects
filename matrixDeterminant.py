4cdf574b79dad697486869ecfca700e7266fa8af
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2023-07-13 21:27:58 -0500

    Create matrixDeterminant.py

---
+++ matrixDeterminant.py
@@ -0,0 +1,15 @@
+""" ===================================================================
+
+**Matrix Determinant Program**
+
+@author: Courtney Brown <cornedtea@proton.me>
+
+"""
+
+
+def findDeterminant():
+    """ Takes in a list of lists (a matrix) and finds the determinant."""
+    
+
+if __name__ == "__main__":
+    pass
